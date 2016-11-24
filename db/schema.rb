f1987f28aa8941caf431251b697a138dca6a6ca6
Sofía Zapata Zavala <sofiazapatazavala@gmail.com>
2016-11-24 10:02:44 -0300

    Más correciones a migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161240145951) do
+ActiveRecord::Schema.define(version: 20161124145951) do
 
   create_table "lessons", force: :cascade do |t|
     t.date     "fecha"
